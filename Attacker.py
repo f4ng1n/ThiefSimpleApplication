ed6da9c480a178c122006ec3d76905ca8ef2014c
Phan Thi Hai Ha <phantransmith@gmail.com>
2020-04-02 03:58:47 +0300

    edit function Copy()
    Tried to solve problem about saving and rename the copied files. (!)

--- Attacker.py
+++ Attacker.py
@@ -18,10 +18,10 @@ def browseBtnPublic():
     os.chdir(askDirectoryTrack)
     src = os.getcwd()
     print("1",src)
+    
     for f in os.listdir():
-       listSrc = os.path.splitext(f)
-       print (f)
-       
+        print(f)
+           
 lbl1 = Label(app,text="Track to public directory:")
 lbl1.grid(column=0, row=0, sticky='W')
 
@@ -40,30 +40,51 @@ def browseBtnAttacker():
     dst = os.getcwd()
     print("2",dst)
     for f in os.listdir():
-        listSrc = os.path.splitext(f)
         print(f)
 lbl3 = Label(app,text="Attacker's directory:")
-lbl3.grid(column=0, row=2, sticky='W')
+lbl3.grid(column=0, row=3, sticky='W')
 
 butBrsAt = Button(app, text='Browse',command=browseBtnAttacker) 
-butBrsAt.grid(column=0, row=3)
+butBrsAt.grid(column=0, row=4)
 dest_path = StringVar()
 lbl4 = Label(app, textvariable=dest_path)
-lbl4.grid(column=1, row=3,sticky='W')
+lbl4.grid(column=1, row=4,sticky='W')
 #==========Start button-Function to start tracking===
 src = "C:/Users/phanh/source/repos/Lab1/public/"
 dst = "C:/Users/phanh/source/repos/Lab1/attacker/"
 
+
 def Track():
     def Copy(src):
-        L = []
+        L = []   
+        
         for root, dirs, files in os.walk(src):
             for file in files:
                 L.append(os.path.join(root,file))
-                shutil.copyfile(os.path.join(root,file),dst+file) 
+                shutil.copyfile(os.path.join(root,file),dst+file)
+                # Get the create time of the file
+                create_time = os.path.getctime(file)
+                # Get the modified time
                 modified_time = os.path.getmtime(file)
+                # Get the date modified time
                 modified_date = datetime.date.fromtimestamp(modified_time)
-                os.rename(file,modified_date.strftime("%d.%m.%Y"+"_"+file))
+                # Get the readable timestamp format
+                format_time = datetime.datetime.fromtimestamp(create_time)
+                # convert time into string
+                format_time_string = format_time.strftime("%d-%m-%Y %H:%M:%S"+"-"+file) # e.g. 01-04-2020 09.03.45-demo.txt
+                # contruct the new name of the file
+                newfile = format_time_string + '.txt'
+
+                # If there is other files created at the same timestamp
+                if (newfile in L.keys()):
+                    index = newFileText[newfile]+1
+                    newFileText[newfile]=index
+                    newfile=format_time_string+'-'+str(index)+"-"+file 
+                else:
+                    newFileText[newfile] = 0
+                # Rename the file
+                os.rename(file,newfile)
+                
        
     Copy(src)
 
